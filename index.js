7821704baf7f11795af053e7e0be2103c73e1437
Shouta Yoshiwatari <shouta_y2012@yahoo.co.jp>
2016-09-08 21:56:54 +0900

    node sample saccess

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-var express = require('express');
-var http = require('http');
-var app = express();
-var fs = require('fs');
-var io = require('socket.io').listen(server);
-
-var server = http.createServer();
-server.on('request',doRequest);
-server.listen(8124);
-console.log('Server running!');
-
-function doRequest(req,res){
-	fs.readFile('./index.html','UTF-8',
-		function(err,data){
-			res.writeHead(200, {'Content-Type':'text/html'});
-			res.write(data);
-			res.end();
-		});
-}
-io.sockets.on('connection',function(socket){
-	console.log("connection");
-socket.on('connected',function(data){
-	console.log("data");
-	io.sockes.emit('publish',{value:data.value});
-});
+var http = require("http");
+var socketio = require("socket.io");
+var fs = require("fs");
+ 
+var server = http.createServer(function(req, res) {
+     res.writeHead(200, {"Content-Type":"text/html"});
+     var output = fs.readFileSync("./index.html", "utf-8");
+     res.end(output);
+}).listen(process.env.VMC_APP_PORT || 3000);
+ 
+var io = socketio.listen(server);
+ 
+io.sockets.on("connection", function (socket) {
+ 
+  // メッセージ送信（送信者にも送られる）
+  socket.on("C_to_S_message", function (data) {
+    io.sockets.emit("S_to_C_message", {value:data.value});
+  });
+ 
+  // ブロードキャスト（送信者以外の全員に送信）
+  socket.on("C_to_S_broadcast", function (data) {
+    socket.broadcast.emit("S_to_C_message", {value:data.value});
+  });
+ 
+  // 切断したときに送信
+  socket.on("disconnect", function () {
+//    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
+  });
 });
